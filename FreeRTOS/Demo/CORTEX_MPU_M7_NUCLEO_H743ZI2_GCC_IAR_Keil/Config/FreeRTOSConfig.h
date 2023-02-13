ad1d8376d517801ee19ded6a41e076b94a4d0c43
Gaurav Aggarwal <aggarg@amazon.com>
2023-02-13 17:10:50 +0530

    Project file and config changes
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- FreeRTOS/Demo/CORTEX_MPU_M7_NUCLEO_H743ZI2_GCC_IAR_Keil/Config/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_MPU_M7_NUCLEO_H743ZI2_GCC_IAR_Keil/Config/FreeRTOSConfig.h
@@ -140,7 +140,7 @@ See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
  * used. TEX=0, S=0, C=1, B=1. */
 #define configTEX_S_C_B_SRAM						( 0x03UL )
 
-#define configPROTECTED_KERNEL_OBJECT_HANDLE_POOL_SIZE				( 20 )
+#define configPROTECTED_KERNEL_OBJECT_HANDLE_POOL_SIZE				( 50 )
 /* Defines for Testing Project https://github.com/FreeRTOS/FreeRTOS/tree/main/FreeRTOS/Demo/ThirdParty/Template*/
 
 #define configTASK_NOTIFICATION_ARRAY_ENTRIES 	3
@@ -149,31 +149,32 @@ See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 
 #define configSTART_TASK_NOTIFY_TESTS             0
 #define configSTART_TASK_NOTIFY_ARRAY_TESTS       0
-#define configSTART_BLOCKING_QUEUE_TESTS          0
-#define configSTART_SEMAPHORE_TESTS               0
-#define configSTART_POLLED_QUEUE_TESTS            0
+#define configSTART_BLOCKING_QUEUE_TESTS          1
+#define configSTART_SEMAPHORE_TESTS               1
+#define configSTART_POLLED_QUEUE_TESTS            1
 #define configSTART_INTEGER_MATH_TESTS            0
-#define configSTART_GENERIC_QUEUE_TESTS           0
-#define configSTART_PEEK_QUEUE_TESTS              0
+#define configSTART_GENERIC_QUEUE_TESTS           1
+#define configSTART_PEEK_QUEUE_TESTS              1
 #define configSTART_MATH_TESTS                    0
-#define configSTART_RECURSIVE_MUTEX_TESTS         0
-#define configSTART_COUNTING_SEMAPHORE_TESTS      0
-#define configSTART_QUEUE_SET_TESTS               0
-#define configSTART_QUEUE_OVERWRITE_TESTS         0
+#define configSTART_RECURSIVE_MUTEX_TESTS         1
+#define configSTART_COUNTING_SEMAPHORE_TESTS      1
+#define configSTART_QUEUE_SET_TESTS               1
+#define configSTART_QUEUE_OVERWRITE_TESTS         1
 #define configSTART_EVENT_GROUP_TESTS             0
 #define configSTART_INTERRUPT_SEMAPHORE_TESTS     1
-#define configSTART_QUEUE_SET_POLLING_TESTS       0
+#define configSTART_QUEUE_SET_POLLING_TESTS       1
 #define configSTART_BLOCK_TIME_TESTS              0
 #define configSTART_ABORT_DELAY_TESTS             0
 #define configSTART_MESSAGE_BUFFER_TESTS          0
 #define configSTART_STREAM_BUFFER_TESTS           0
 #define configSTART_STREAM_BUFFER_INTERRUPT_TESTS 0
 #define configSTART_TIMER_TESTS                   0
-#define configSTART_INTERRUPT_QUEUE_TESTS         0
+#define configSTART_INTERRUPT_QUEUE_TESTS         1
 #define configSTART_REGISTER_TESTS                0
 #define configSTART_DELETE_SELF_TESTS             0
 
 #define configALLOW_UNPRIVILEGED_CRITICAL_SECTIONS 1 /* PolledQ tests need it. */
+#define configUSE_QUEUE_SETS 1
 /*------------------------------------------------------------------------------------------------------------*/
 
 #endif /* FREERTOS_CONFIG_H */
