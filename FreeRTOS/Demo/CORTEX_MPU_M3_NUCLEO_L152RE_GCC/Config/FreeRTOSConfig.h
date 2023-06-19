400045635dd65d55479f2a6c1482abc06f697f4f
kar-rahul-aws <karahulx@amazon.com>
2023-06-19 19:41:35 +0530

    Change FreeRTOSConfig.h file

--- FreeRTOS/Demo/CORTEX_MPU_M3_NUCLEO_L152RE_GCC/Config/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_MPU_M3_NUCLEO_L152RE_GCC/Config/FreeRTOSConfig.h
@@ -27,6 +27,10 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /*-----------------------------------------------------------
  * Application specific definitions.
  *
@@ -86,7 +90,7 @@
 #define configUSE_TIMERS                            1
 #define configTIMER_TASK_PRIORITY                   ( 2 )
 #define configTIMER_QUEUE_LENGTH                    10
-#define configTIMER_TASK_STACK_DEPTH                256
+#define configTIMER_TASK_STACK_DEPTH                ( configMINIMAL_STACK_SIZE )
 
 /* Set the following definitions to 1 to include the API function, or zero
  * to exclude the API function. */
@@ -115,7 +119,7 @@
 
 /* The lowest interrupt priority that can be used in a call to a "set priority"
  * function. */
-#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY     15
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY     0xf
 
 /* The highest interrupt priority that can be used by any interrupt service
  * routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT
