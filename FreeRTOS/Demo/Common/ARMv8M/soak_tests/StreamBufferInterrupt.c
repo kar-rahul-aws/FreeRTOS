176c15bb1499886526141407837431b673d51029
kar-rahul-aws <karahulx@amazon.com>
2023-06-22 11:16:24 +0530

    Add soak tests for nRF9160_SES NTZ Demo

---
+++ FreeRTOS/Demo/Common/ARMv8M/soak_tests/StreamBufferInterrupt.c
@@ -0,0 +1,254 @@
+/*
+ * FreeRTOS V202212.00
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
+ *
+ */
+
+/*
+ * A simple example that shows a stream buffer being used to pass data from an
+ * interrupt to a task.
+ *
+ * There are two strings, pcStringToSend and pcStringToReceive, where
+ * pcStringToReceive is a substring of pcStringToSend.  The interrupt sends
+ * a few bytes of pcStringToSend to a stream buffer ever few times that it
+ * executes.  A task reads the bytes from the stream buffer, looking for the
+ * substring, and flagging an error if the received data is invalid.
+ */
+
+/* Standard includes. */
+#include "stdio.h"
+#include "string.h"
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "stream_buffer.h"
+
+/* Demo app includes. */
+#include "StreamBufferInterrupt.h"
+
+#define sbiSTREAM_BUFFER_LENGTH_BYTES        ( ( size_t ) 100 )
+#define sbiSTREAM_BUFFER_TRIGGER_LEVEL_10    ( ( BaseType_t ) 10 )
+
+#define streambufferSHARED_MEM_SIZE_WORDS     ( 8 )
+#define streambufferSHARED_MEM_SIZE_BYTES     ( 32 )
+
+/*-----------------------------------------------------------*/
+
+/* Implements the task that receives a stream of bytes from the interrupt. */
+static void prvReceivingTask( void * pvParameters );
+
+/*-----------------------------------------------------------*/
+
+/* The stream buffer that is used to send data from an interrupt to the task. */
+static StreamBufferHandle_t xStreamBuffer[ streambufferSHARED_MEM_SIZE_WORDS ] __attribute__( ( aligned( streambufferSHARED_MEM_SIZE_BYTES ) ) ) = { NULL };
+
+/* The string that is sent from the interrupt to the task four bytes at a
+ * time.  Must be multiple of 4 bytes long as the ISR sends 4 bytes at a time*/
+static const char * pcStringToSend = "_____Hello FreeRTOS_____";
+
+/* The string to task is looking for, which must be a substring of
+ * pcStringToSend. */
+static const char * pcStringToReceive[ streambufferSHARED_MEM_SIZE_WORDS ] __attribute__( ( aligned( streambufferSHARED_MEM_SIZE_BYTES ) ) ) = { "Hello FreeRTOS" };
+
+/* Set to pdFAIL if anything unexpected happens. */
+static BaseType_t xDemoStatus[ streambufferSHARED_MEM_SIZE_WORDS ] __attribute__( ( aligned( streambufferSHARED_MEM_SIZE_BYTES ) ) ) = { pdPASS };
+
+/* Incremented each time pcStringToReceive is correctly received, provided no
+ * errors have occurred.  Used so the check task can check this task is still
+ * running as expected. */
+static uint32_t ulCycleCount[ streambufferSHARED_MEM_SIZE_WORDS ] __attribute__( ( aligned( streambufferSHARED_MEM_SIZE_BYTES ) ) ) = { 0 };;
+
+/*-----------------------------------------------------------*/
+
+void vStartStreamBufferInterruptDemo( void )
+{
+    static StackType_t xReceivingTaskStack[ configMINIMAL_STACK_SIZE ] __attribute__( ( aligned( configMINIMAL_STACK_SIZE * sizeof( StackType_t ) ) ) );
+
+    /* Create the stream buffer that sends data from the interrupt to the
+     * task, and create the task. */
+    xStreamBuffer[ 0 ] = xStreamBufferCreate( sbiSTREAM_BUFFER_LENGTH_BYTES, /* The buffer length in bytes. */
+                                              sbiSTREAM_BUFFER_TRIGGER_LEVEL_10 /* The stream buffer's trigger level. */
+                                            );
+
+    TaskParameters_t xReceivingTaskParameters =
+    {
+        .pvTaskCode      = prvReceivingTask,
+        .pcName          = "StrIntRx",
+        .usStackDepth    = configMINIMAL_STACK_SIZE,
+        .pvParameters    = NULL,
+        .uxPriority      = tskIDLE_PRIORITY + 2,
+        .puxStackBuffer  = xReceivingTaskStack,
+        .xRegions        =    {
+                                { ( void * ) &( xDemoStatus[ 0 ] ), streambufferSHARED_MEM_SIZE_BYTES,
+                                  ( portMPU_REGION_READ_WRITE | portMPU_REGION_EXECUTE_NEVER )
+                                },
+                                { ( void * ) &( ulCycleCount[ 0 ] ), streambufferSHARED_MEM_SIZE_BYTES,
+                                  ( portMPU_REGION_READ_WRITE | portMPU_REGION_EXECUTE_NEVER )
+                                },
+                                { ( void * ) &( pcStringToReceive[ 0 ] ), streambufferSHARED_MEM_SIZE_BYTES,
+                                  ( portMPU_REGION_READ_WRITE | portMPU_REGION_EXECUTE_NEVER )
+                                },
+                                { ( void * ) &( xStreamBuffer[ 0 ] ), streambufferSHARED_MEM_SIZE_BYTES,
+                                  ( portMPU_REGION_READ_WRITE | portMPU_REGION_EXECUTE_NEVER )
+                                },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        }
+                            }
+    };
+
+    xTaskCreateRestricted( &( xReceivingTaskParameters ), NULL );
+}
+/*-----------------------------------------------------------*/
+
+static void prvReceivingTask( void * pvParameters )
+{
+    char cRxBuffer[ 20 ];
+    BaseType_t xNextByte = 0;
+
+    /* Remove warning about unused parameters. */
+    ( void ) pvParameters;
+
+    /* Make sure the string will fit in the Rx buffer, including the NULL
+     * terminator. */
+    configASSERT( sizeof( cRxBuffer ) > strlen( pcStringToReceive[ 0 ] ) );
+
+    /* Make sure the stream buffer has been created. */
+    configASSERT( xStreamBuffer[ 0 ] != NULL );
+
+    /* Start with the Rx buffer in a known state. */
+    memset( cRxBuffer, 0x00, sizeof( cRxBuffer ) );
+
+    for( ; ; )
+    {
+        /* Keep receiving characters until the end of the string is received.
+         * Note:  An infinite block time is used to simplify the example.  Infinite
+         * block times are not recommended in production code as they do not allow
+         * for error recovery. */
+        xStreamBufferReceive( xStreamBuffer[ 0 ], /* The stream buffer data is being received from. */
+                              ( void * ) &( cRxBuffer[ xNextByte ] ), /* Where to place received data. */
+                              sizeof( char ), /* The number of bytes to receive. */
+                              portMAX_DELAY /* The time to wait for the next data if the buffer is empty. */
+                            );
+
+        /* If xNextByte is 0 then this task is looking for the start of the
+         * string, which is 'H'. */
+        if( xNextByte == 0 )
+        {
+            if( cRxBuffer[ xNextByte ] == 'H' )
+            {
+                /* The start of the string has been found.  Now receive
+                 * characters until the end of the string is found. */
+                xNextByte++;
+            }
+        }
+        else
+        {
+            /* Receiving characters while looking for the end of the string,
+             * which is an 'S'. */
+            if( cRxBuffer[ xNextByte ] == 'S' )
+            {
+                /* The string has now been received.  Check its validity. */
+                if( strcmp( cRxBuffer, pcStringToReceive[ 0 ] ) != 0 )
+                {
+                    xDemoStatus[ 0 ] = pdFAIL;
+                }
+
+                /* Return to start looking for the beginning of the string
+                 * again. */
+                memset( cRxBuffer, 0x00, sizeof( cRxBuffer ) );
+                xNextByte = 0;
+
+                /* Increment the cycle count as an indication to the check task
+                 * that this demo is still running. */
+                if( xDemoStatus[ 0 ] == pdPASS )
+                {
+                    ulCycleCount[ 0 ]++;
+                }
+            }
+            else
+            {
+                /* Receive the next character the next time around, while
+                 * continuing to look for the end of the string. */
+                xNextByte++;
+
+                configASSERT( ( size_t ) xNextByte < sizeof( cRxBuffer ) );
+            }
+        }
+    }
+}
+/*-----------------------------------------------------------*/
+
+void vBasicStreamBufferSendFromISR( void )
+{
+    static size_t xNextByteToSend = 0;
+    const BaseType_t xCallsBetweenSends = 100, xBytesToSend = 4;
+    static BaseType_t xCallCount = 0;
+
+    /* Is it time to write to the stream buffer again? */
+    xCallCount++;
+
+    if( xCallCount > xCallsBetweenSends )
+    {
+        xCallCount = 0;
+
+        /* Send the next four bytes to the stream buffer. */
+        xStreamBufferSendFromISR( xStreamBuffer[ 0 ],
+                                  ( const void * ) ( pcStringToSend + xNextByteToSend ),
+                                  xBytesToSend,
+                                  NULL );
+
+        /* Send the next four bytes the next time around, wrapping to the start
+         * of the string if necessary. */
+        xNextByteToSend += xBytesToSend;
+
+        if( xNextByteToSend >= strlen( pcStringToSend ) )
+        {
+            xNextByteToSend = 0;
+        }
+    }
+}
+/*-----------------------------------------------------------*/
+
+BaseType_t xIsInterruptStreamBufferDemoStillRunning( void )
+{
+    uint32_t ulLastCycleCount = 0;
+
+    /* Check the demo is still running. */
+    if( ulLastCycleCount == ulCycleCount[ 0 ] )
+    {
+        xDemoStatus[ 0 ] = pdFAIL;
+    }
+    else
+    {
+        ulLastCycleCount = ulCycleCount[ 0 ];
+    }
+
+    return xDemoStatus[ 0 ];
+}
+/*-----------------------------------------------------------*/
---
+++ FreeRTOS/Demo/Common/ARMv8M/soak_tests/countsem.c
@@ -0,0 +1,353 @@
+/*
+ * FreeRTOS V202212.00
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
+ *
+ */
+
+
+/*
+ * Simple demonstration of the usage of counting semaphore.
+ */
+
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+/* Demo program include files. */
+#include "countsem.h"
+
+#define countSHARED_MEM_SIZE_WORDS             ( 8 )
+#define countSHARED_MEM_SIZE_BYTES             ( 32 )
+
+/* The maximum count value that the semaphore used for the demo can hold. */
+#define countMAX_COUNT_VALUE       ( 200 )
+
+/* Constants used to indicate whether or not the semaphore should have been
+ * created with its maximum count value, or its minimum count value.  These
+ * numbers are used to ensure that the pointers passed in as the task parameters
+ * are valid. */
+#define countSTART_AT_MAX_COUNT    ( 0xaa )
+#define countSTART_AT_ZERO         ( 0x55 )
+
+/* Two tasks are created for the test.  One uses a semaphore created with its
+ * count value set to the maximum, and one with the count value set to zero. */
+#define countNUM_TEST_TASKS        ( 2 )
+#define countDONT_BLOCK            ( 0 )
+
+/*-----------------------------------------------------------*/
+
+/* Flag that will be latched to pdTRUE should any unexpected behavior be
+ * detected in any of the tasks. */
+static volatile BaseType_t xErrorDetected[ countSHARED_MEM_SIZE_WORDS ] __attribute__( ( aligned( countSHARED_MEM_SIZE_BYTES ) ) ) = { pdFALSE };
+
+/*-----------------------------------------------------------*/
+
+/*
+ * The demo task.  This simply counts the semaphore up to its maximum value,
+ * the counts it back down again.  The result of each semaphore 'give' and
+ * 'take' is inspected, with an error being flagged if it is found not to be
+ * the expected result.
+ */
+static void prvCountingSemaphoreTask( void * pvParameters );
+
+/*
+ * Utility function to increment the semaphore count value up from zero to
+ * countMAX_COUNT_VALUE.
+ */
+static void prvIncrementSemaphoreCount( SemaphoreHandle_t xSemaphore,
+                                        volatile UBaseType_t * puxLoopCounter );
+
+/*
+ * Utility function to decrement the semaphore count value up from
+ * countMAX_COUNT_VALUE to zero.
+ */
+static void prvDecrementSemaphoreCount( SemaphoreHandle_t xSemaphore,
+                                        volatile UBaseType_t * puxLoopCounter );
+
+/*-----------------------------------------------------------*/
+
+/* The structure that is passed into the task as the task parameter. */
+typedef struct COUNT_SEM_STRUCT
+{
+    /* The semaphore to be used for the demo. */
+    SemaphoreHandle_t xSemaphore;
+
+    /* Set to countSTART_AT_MAX_COUNT if the semaphore should be created with
+     * its count value set to its max count value, or countSTART_AT_ZERO if it
+     * should have been created with its count value set to 0. */
+    UBaseType_t uxExpectedStartCount;
+
+    /* Incremented on each cycle of the demo task.  Used to detect a stalled
+     * task. */
+    volatile UBaseType_t uxLoopCounter;
+
+    uint32_t unused[ 5 ];
+} xCountSemStruct;
+
+/* Two structures are defined, one is passed to each test task. */
+static xCountSemStruct xParameters1 __attribute__( ( aligned( countSHARED_MEM_SIZE_BYTES ) ) );
+static xCountSemStruct xParameters2 __attribute__( ( aligned( countSHARED_MEM_SIZE_BYTES ) ) );
+
+/*-----------------------------------------------------------*/
+
+void vStartCountingSemaphoreTasks( void )
+{
+    /* Create the semaphores that we are going to use for the test/demo.  The
+     * first should be created such that it starts at its maximum count value,
+     * the second should be created such that it starts with a count value of zero. */
+    xParameters1.xSemaphore = xSemaphoreCreateCounting( countMAX_COUNT_VALUE, countMAX_COUNT_VALUE );
+    xParameters1.uxExpectedStartCount = countSTART_AT_MAX_COUNT;
+    xParameters1.uxLoopCounter = 0;
+
+    xParameters2.xSemaphore = xSemaphoreCreateCounting( countMAX_COUNT_VALUE, 0 );
+    xParameters2.uxExpectedStartCount = 0;
+    xParameters2.uxLoopCounter = 0;
+
+    static StackType_t xCountingSemaphoreTaskStack1[ configMINIMAL_STACK_SIZE ] __attribute__( ( aligned( configMINIMAL_STACK_SIZE * sizeof( StackType_t ) ) ) );
+    static StackType_t xCountingSemaphoreTaskStack2[ configMINIMAL_STACK_SIZE ]__attribute__( ( aligned( configMINIMAL_STACK_SIZE * sizeof( StackType_t ) ) ) );
+
+    TaskParameters_t xCountingSemaphoreTask1 =
+    {
+        .pvTaskCode      = prvCountingSemaphoreTask,
+        .pcName          = "CNT1",
+        .usStackDepth    = configMINIMAL_STACK_SIZE,
+        .pvParameters    = ( void * ) &( xParameters1 ),
+        .uxPriority      = tskIDLE_PRIORITY,
+        .puxStackBuffer  = xCountingSemaphoreTaskStack1,
+        .xRegions        =  {
+                                { ( void * ) &( xErrorDetected[ 0 ] ), countSHARED_MEM_SIZE_BYTES,
+                                  ( portMPU_REGION_READ_WRITE | portMPU_REGION_EXECUTE_NEVER )
+                                },
+                                { &( xParameters1 ), countSHARED_MEM_SIZE_BYTES,
+                                  ( portMPU_REGION_READ_WRITE | portMPU_REGION_EXECUTE_NEVER )
+                                },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        }
+                            }
+    };
+    TaskParameters_t xCountingSemaphoreTask2 =
+    {
+        .pvTaskCode      = prvCountingSemaphoreTask,
+        .pcName          = "CNT2",
+        .usStackDepth    = configMINIMAL_STACK_SIZE,
+        .pvParameters    = ( void * ) &( xParameters2 ),
+        .uxPriority      = tskIDLE_PRIORITY,
+        .puxStackBuffer  = xCountingSemaphoreTaskStack2,
+        .xRegions        =  {
+                                { ( void * ) &( xErrorDetected[ 0 ] ), countSHARED_MEM_SIZE_BYTES,
+                                  ( portMPU_REGION_READ_WRITE | portMPU_REGION_EXECUTE_NEVER )
+                                },
+                                { &( xParameters2 ), countSHARED_MEM_SIZE_BYTES,
+                                  ( portMPU_REGION_READ_WRITE | portMPU_REGION_EXECUTE_NEVER )
+                                },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        },
+                                { 0,                0,                    0                                                        }
+                            }
+    };
+
+    /* Were the semaphores created? */
+    if( ( xParameters1.xSemaphore != NULL ) || ( xParameters2.xSemaphore != NULL ) )
+    {
+        /* vQueueAddToRegistry() adds the semaphore to the registry, if one is
+         * in use.  The registry is provided as a means for kernel aware
+         * debuggers to locate semaphores and has no purpose if a kernel aware
+         * debugger is not being used.  The call to vQueueAddToRegistry() will be
+         * removed by the pre-processor if configQUEUE_REGISTRY_SIZE is not
+         * defined or is defined to be less than 1. */
+        vQueueAddToRegistry( ( QueueHandle_t ) xParameters1.xSemaphore, "Counting_Sem_1" );
+        vQueueAddToRegistry( ( QueueHandle_t ) xParameters2.xSemaphore, "Counting_Sem_2" );
+
+        /* Create the demo tasks, passing in the semaphore to use as the parameter. */
+        xTaskCreateRestricted( &( xCountingSemaphoreTask1 ), NULL );
+        xTaskCreateRestricted( &( xCountingSemaphoreTask2 ), NULL );
+    }
+}
+/*-----------------------------------------------------------*/
+
+static void prvDecrementSemaphoreCount( SemaphoreHandle_t xSemaphore,
+                                        volatile UBaseType_t * puxLoopCounter )
+{
+    UBaseType_t ux;
+
+    /* If the semaphore count is at its maximum then we should not be able to
+     * 'give' the semaphore. */
+    if( xSemaphoreGive( xSemaphore ) == pdPASS )
+    {
+        xErrorDetected[ 0 ] = pdTRUE;
+    }
+
+    /* We should be able to 'take' the semaphore countMAX_COUNT_VALUE times. */
+    for( ux = 0; ux < countMAX_COUNT_VALUE; ux++ )
+    {
+        configASSERT( uxSemaphoreGetCount( xSemaphore ) == ( countMAX_COUNT_VALUE - ux ) );
+
+        if( xSemaphoreTake( xSemaphore, countDONT_BLOCK ) != pdPASS )
+        {
+            /* We expected to be able to take the semaphore. */
+            xErrorDetected[ 0 ] = pdTRUE;
+        }
+
+        ( *puxLoopCounter )++;
+    }
+
+    #if configUSE_PREEMPTION == 0
+        taskYIELD();
+    #endif
+
+    /* If the semaphore count is zero then we should not be able to    'take'
+     * the semaphore. */
+    configASSERT( uxSemaphoreGetCount( xSemaphore ) == 0 );
+
+    if( xSemaphoreTake( xSemaphore, countDONT_BLOCK ) == pdPASS )
+    {
+        xErrorDetected[ 0 ] = pdTRUE;
+    }
+}
+/*-----------------------------------------------------------*/
+
+static void prvIncrementSemaphoreCount( SemaphoreHandle_t xSemaphore,
+                                        volatile UBaseType_t * puxLoopCounter )
+{
+    UBaseType_t ux;
+
+    /* If the semaphore count is zero then we should not be able to    'take'
+     * the semaphore. */
+    if( xSemaphoreTake( xSemaphore, countDONT_BLOCK ) == pdPASS )
+    {
+        xErrorDetected[ 0 ] = pdTRUE;
+    }
+
+    /* We should be able to 'give' the semaphore countMAX_COUNT_VALUE times. */
+    for( ux = 0; ux < countMAX_COUNT_VALUE; ux++ )
+    {
+        configASSERT( uxSemaphoreGetCount( xSemaphore ) == ux );
+
+        if( xSemaphoreGive( xSemaphore ) != pdPASS )
+        {
+            /* We expected to be able to take the semaphore. */
+            xErrorDetected[ 0 ] = pdTRUE;
+        }
+
+        ( *puxLoopCounter )++;
+    }
+
+    #if configUSE_PREEMPTION == 0
+        taskYIELD();
+    #endif
+
+    /* If the semaphore count is at its maximum then we should not be able to
+     * 'give' the semaphore. */
+    if( xSemaphoreGive( xSemaphore ) == pdPASS )
+    {
+        xErrorDetected[ 0 ] = pdTRUE;
+    }
+}
+/*-----------------------------------------------------------*/
+
+static void prvCountingSemaphoreTask( void * pvParameters )
+{
+    xCountSemStruct * pxParameter;
+
+    #ifdef USE_STDIO
+        void vPrintDisplayMessage( const char * const * ppcMessageToSend );
+
+        const char * const pcTaskStartMsg = "Counting semaphore demo started.\r\n";
+
+        /* Queue a message for printing to say the task has started. */
+        vPrintDisplayMessage( &pcTaskStartMsg );
+    #endif
+
+    /* The semaphore to be used was passed as the parameter. */
+    pxParameter = ( xCountSemStruct * ) pvParameters;
+
+    /* Did we expect to find the semaphore already at its max count value, or
+     * at zero? */
+    if( pxParameter->uxExpectedStartCount == countSTART_AT_MAX_COUNT )
+    {
+        prvDecrementSemaphoreCount( pxParameter->xSemaphore, &( pxParameter->uxLoopCounter ) );
+    }
+
+    /* Now we expect the semaphore count to be 0, so this time there is an
+     * error if we can take the semaphore. */
+    if( xSemaphoreTake( pxParameter->xSemaphore, 0 ) == pdPASS )
+    {
+        xErrorDetected[ 0 ] = pdTRUE;
+    }
+
+    for( ; ; )
+    {
+        prvIncrementSemaphoreCount( pxParameter->xSemaphore, &( pxParameter->uxLoopCounter ) );
+        prvDecrementSemaphoreCount( pxParameter->xSemaphore, &( pxParameter->uxLoopCounter ) );
+    }
+}
+/*-----------------------------------------------------------*/
+
+BaseType_t xAreCountingSemaphoreTasksStillRunning( void )
+{
+    static UBaseType_t uxLastCount0 = 0, uxLastCount1 = 0;
+    BaseType_t xReturn = pdPASS;
+
+    /* Return fail if any 'give' or 'take' did not result in the expected
+     * behaviour. */
+    if( xErrorDetected[ 0 ] != pdFALSE )
+    {
+        xReturn = pdFAIL;
+    }
+
+    /* Return fail if either task is not still incrementing its loop counter. */
+    if( uxLastCount0 == xParameters1.uxLoopCounter )
+    {
+        xReturn = pdFAIL;
+    }
+    else
+    {
+        uxLastCount0 = xParameters1.uxLoopCounter;
+    }
+
+    if( uxLastCount1 == xParameters2.uxLoopCounter )
+    {
+        xReturn = pdFAIL;
+    }
+    else
+    {
+        uxLastCount1 = xParameters2.uxLoopCounter;
+    }
+
+    return xReturn;
+}
+/*-----------------------------------------------------------*/
