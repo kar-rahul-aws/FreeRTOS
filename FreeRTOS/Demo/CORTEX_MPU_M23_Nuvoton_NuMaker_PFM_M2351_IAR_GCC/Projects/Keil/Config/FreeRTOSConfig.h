8ebb5675384592f35446b1f734c005cc62e38ffe
kar-rahul-aws <karahulx@amazon.com>
2023-06-27 20:05:45 +0530

    Add Reg tests for Nuvoton Numaker M23 GCC

--- FreeRTOS/Demo/CORTEX_MPU_M23_Nuvoton_NuMaker_PFM_M2351_IAR_GCC/Projects/Keil/Config/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_MPU_M23_Nuvoton_NuMaker_PFM_M2351_IAR_GCC/Projects/Keil/Config/FreeRTOSConfig.h
@@ -209,8 +209,8 @@
 #define configSTART_STREAM_BUFFER_TESTS               1
 #define configSTART_STREAM_BUFFER_INTERRUPT_TESTS     1
 #define configSTART_TIMER_TESTS                       1
-#define configSTART_INTERRUPT_QUEUE_TESTS             0
-#define configSTART_REGISTER_TESTS                    0
+#define configSTART_INTERRUPT_QUEUE_TESTS             1
+#define configSTART_REGISTER_TESTS                    1
 #define configSTART_DELETE_SELF_TESTS                 0
 
 #define configALLOW_UNPRIVILEGED_CRITICAL_SECTIONS    1 /* PolledQ tests need it. */
